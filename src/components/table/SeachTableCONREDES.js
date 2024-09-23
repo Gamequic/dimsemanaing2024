b9159b974454c2c1780adc937d14da4f3e719015
Demian Calleros <demiancalleros0@gmail.com>
2024-09-22 18:03:54 -0600

    feat(conredes): filter for conredes

--- src/components/table/SeachTableCONREDES.js
+++ src/components/table/SeachTableCONREDES.js
@@ -1,98 +1,50 @@
-import React, { useMemo } from 'react';
-// import { useTable, useGlobalFilter } from 'react-table';
+import { useState, useMemo } from "react";
 
-import { useTable } from 'react-table';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import SearchTable from "./SeachTable";
 
+const SearchTableConcursos = ({ data, title, children }) => {
+    const [visibleColumns, setVisibleColumns] = useState({
+        PROGRAMA: true,
+        NOMBRE: true,
+        FECHA: true,
+        LUGAR: true,
+        ORGANIZADORES: false,
+        // Liga: true,
+      });
+    
+      const columns = useMemo(
+        () => [
+          { Header: 'Conferencia', accessor: 'CONFERENCIA', show: visibleColumns.PROGRAMA },
+          { Header: 'Fecha', accessor: 'FECHA', show: visibleColumns.NOMBRE },
+          { Header: 'Hora', accessor: 'HORA', show: visibleColumns.FECHA },
+          { Header: 'Lugar', accessor: 'LUGAR', show: visibleColumns.LUGAR },
+          { Header: 'Conferencista', accessor: 'CONFERENCISTA', show: visibleColumns.ORGANIZADORES },
+        //   { 
+        //     Header: 'Liga', 
+        //     accessor: 'Liga',
+        //     show: visibleColumns.Liga,
+        //     Cell: ({ value }) => (
+        //       value && value.startsWith('http') ? (
+        //         <a href={value} target="_blank" rel="noopener noreferrer">{value}</a>
+        //       ) : (
+        //         value
+        //       )
+        //     ),
+        //   },
+        ].filter(column => column.show),
+        [visibleColumns]
+      );
 
-import './SearchTable.css'
+    return (
+        <SearchTable
+            children={children}
+            title={title}
+            data={data}
+            visibleColumns={visibleColumns}
+            setVisibleColumns={setVisibleColumns}
+            columns={columns}
+        />
+     )
+}
 
-const SearchTableCONREDES = ({ data, title, children }) => {
-  // const [searchInput, setSearchInput] = useState('');
-
-  // Define columns
-  const columns = useMemo(
-    () => [
-      { Header: 'Conferencia', accessor: 'CONFERENCIA' },
-      { Header: 'Fecha', accessor: 'FECHA' },
-      { Header: 'Hora', accessor: 'HORA' },
-      { Header: 'Lugar', accessor: 'LUGAR' },
-      { Header: 'Conferencista', accessor: 'CONFERENCISTA' }
-    ],
-    []
-  );  
-
-  // Set up table with data and columns
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-    // setGlobalFilter,
-  // } = useTable({ columns, data }, useGlobalFilter);
-  } = useTable({ columns, data });
-
-  // Handle search input
-  // const handleSearch = (e) => {
-  //   const value = e.target.value || '';
-  //   setSearchInput(value);
-  //   setGlobalFilter(value);
-  // };
-
-  return (
-    <>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1-content"
-          id="panel1-header"
-        >
-          {title}
-        </AccordionSummary>
-        <AccordionDetails>
-          <div className="table-container">
-            {/* Search input */}
-            {/* <input
-              value={searchInput}
-              onChange={handleSearch}
-              placeholder="Search..."
-              className="search-input"
-            /> */}
-
-            {/* Table */}
-            <table {...getTableProps()} className="styled-table">
-              <thead>
-                {headerGroups.map(headerGroup => (
-                  <tr {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map(column => (
-                      <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                    ))}
-                  </tr>
-                ))}
-              </thead>
-              <tbody {...getTableBodyProps()}>
-                {rows.map(row => {
-                  prepareRow(row);
-                  return (
-                    <tr {...row.getRowProps()}>
-                      {row.cells.map(cell => (
-                        <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                      ))}
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-          {children}
-        </AccordionDetails>
-      </Accordion>
-    </>
-  );
-};
-
-export default SearchTableCONREDES;
+export default SearchTableConcursos;
